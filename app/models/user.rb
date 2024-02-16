f1989b174add6790f03af513bf9d697652765f82
ricardogontarz <ricardopolar2004@gmail.com>
2024-02-16 16:34:37 -0300

    Revoke registration function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, registerable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable #, :registerable
 end
--- db/migrate/20240108165919_create_blog_posts.rb
+++ db/migrate/20240108165919_create_blog_posts.rb
@@ -2,8 +2,7 @@ class CreateBlogPosts < ActiveRecord::Migration[7.0]
   def change
     create_table :blog_posts do |t|
       t.string :title
-      t.text :body
-
+      #t.text :body
       t.timestamps
     end
   end
