03743ebb05b685407324aa24ad2d83abb9405971
ricardogontarz <ricardopolar2004@gmail.com>
2024-02-19 15:36:46 -0300

    Cover image controller

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,5 +1,7 @@
 class BlogPost < ApplicationRecord
+  has_one_attached :cover_image
   has_rich_text :content
+
 
   validates :title, presence: true
   validates :content, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :blog_posts
+  resources :blog_posts do
+    resources :cover_image, only: [:destroy], module: :blog_posts
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog_posts#index"
